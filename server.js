937b61cc33281c9bf45f3222a8947ea8054f0f2a
Masah <s1343157@gmail.com>
2026-02-16 21:44:57 +0900

    fix port for render

--- server.js
+++ server.js
@@ -281,6 +281,8 @@ app.post("/recheck", express.json(), (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
